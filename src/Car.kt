e7aa9d94e9a31d9592860a575299339e69da856f
Subhra Roy <subhra.roy@e-arc.com>
2019-07-22 23:57:52 +0530

    Operator

--- src/Car.kt
+++ src/Car.kt
@@ -6,4 +6,22 @@
 class Car {
 
     var name : String = "Audi"
+
+    fun addNewCarName(carName : String, modelName : String) : String {
+
+        return  carName + "_" + modelName
+
+    }
+
+    fun checkGreatestNumber(firstNum : Int, secondNum : Int) : Int{
+
+        val number = when {
+
+            (firstNum > secondNum) -> { firstNum }
+            else -> { secondNum }
+        }
+
+        return  number
+    }
+
 }
