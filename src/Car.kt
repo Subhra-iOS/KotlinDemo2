ac0bc34f2aa61251566a37f35cfba8946ffdaea7
Subhra Roy <subhra.roy@e-arc.com>
2019-07-23 11:05:31 +0530

    new fun added

--- src/Car.kt
+++ src/Car.kt
@@ -7,4 +7,21 @@ class Car {
 
     var name : String = "Audi"
 
+    fun findGreatestNumber(firstNum : Int, secondNum : Int) : Int{
+
+        val num = when {
+
+            firstNum > secondNum -> { firstNum }
+            else -> { secondNum }
+
+        }
+
+        return  num
+    }
+
+    fun appenCarModelWith(carName : String, modelNo : String) : String{
+
+        return  carName + "_" + modelNo
+    }
+
 }
--- src/Root.kt
+++ src/Root.kt
@@ -8,6 +8,15 @@ fun main(args : Array<String>){
     var car : Car = Car()
     car.name = "BMW"
 
-    print("New Car Name : ${car.name}")
+    print("New Car Name : ${car.name}\n")
 
+    val num1 = 5
+    val num2 = 8
+    val greatestNum = car.findGreatestNumber(num1, num2)
+
+    print("Gretaest Number between $num1 and $num2 is $greatestNum \n")
+
+    val newModelName = car.appenCarModelWith("BMW", "Lux1")
+
+    print("New car model name : $newModelName")
 }
